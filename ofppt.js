ca831dfad14e3a07dd2b42156909635df0c433d1
Abdallah <abdellahsadik44@gmail.com>
2024-05-14 13:29:40 +0100

    ofppt names

---
+++ ofppt.js
@@ -0,0 +1,24 @@
+const centres = [
+  { Centre: "Centre de Formation Professionnelle Multiservices Tit Mlil" },
+  { Centre: "Centre d'Accueil Casablanca" },
+  { Centre: "Centre de Formation et d'Insertion des Femmes et des Jeunes -Tit Melil" },
+  { Centre: "Centre de Qualification Professionnelle Derb El Kabir Casablanca" },
+  { Centre: "Centre de Qualification Professionnelle Essafa Casablanca" },
+  { Centre: "Centre de Recherche et d'Engineering de l'Automobile Casablanca" },
+  { Centre: "Centre Socio-Culturel de Formation et d'Insertion des Jeunes El Hraouine Mediouna" },
+  { Centre: "Espace Socio-Educatif Madinat Errahma Nouaceur" },
+  { Centre: "Institut des Métiers des Vêtements Derb Ghallef Casablanca" },
+  { Centre: "Institut spécialisé de l'Hôtellerie et de la Restauration Casablanca" },
+  { Centre: "Institut Spécialisé de Technologie Appliquée Ain Chok-Inara Casablanca" },
+  { Centre: "Institut Spécialisé de Technologie Appliquée de Confection Sidi Maarouf Casablanca" },
+  { Centre: "Institut Spécialisé de Technologie Appliquée El Hank Casablanca" },
+  { Centre: "Institut Spécialisé de Technologie Appliquée Génie Mécanique Casablanca" },
+  { Centre: "Institut Spécialisé de Technologie Appliquée Hay Hassani 2 Casablanca" },
+  { Centre: "Institut Spécialisé de Technologie Appliquée Lissasfa Casablanca" },
+  { Centre: "Institut Spécialisé de Technologie Appliquée Mediouna Casablanca" },
+  { Centre: "Institut Spécialisé de Technologie Appliquée Polo Casablanca" },
+  { Centre: "Institut spécialisé des Métiers du Cuir Casablanca" },
+  { Centre: "Institut Spécialisé du Bâtiment et des Travaux Publics Hay Errahma Casablanca" }
+];
+
+
