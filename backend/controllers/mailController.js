c30de2c71c316850b6d5bafb02e71028f1f8fbac
Saad <saadksioui@gmail.com>
2024-05-16 12:11:31 +0100

    some updates

--- backend/controllers/mailController.js
+++ backend/controllers/mailController.js
@@ -14,7 +14,7 @@ const sendMail = async (req, res) => {
     //sendmail
     await transporter.sendMail({
       from: email, // you email
-      to: 'ofppt.estage@gmail.com', // to email
+      to: 'multiversal.guru@gmail.com', // to email
       subject: sujet,
       text: body
     });
